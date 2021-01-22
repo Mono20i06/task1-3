597da7aedda1c761d6b21dc000499b37b3e3a2ca
mono20 <h9NDBCGU>
2021-01-22 22:44:40 +0300

    https://github.com/Mono20i06/task1-3.git

---
+++ task1-3.py
@@ -0,0 +1,7 @@
+neighbor_1 = input("Age of the first neighbor: ")
+neighbor_2 = input("Age of the second neighbor: ")
+neighbor_3 = input("Age of the third neighbor: ")
+x = int(neighbor_1) +  int(neighbor_2) + int (neighbor_3)
+print("Summ ages neighbors: " + str(x))
+y = int(neighbor_1) + int(neighbor_2) + int(neighbor_3) / 3
+print("Average age of neighbors: " + str(int(y)))
